6a797fb8575ad87bde5000eacc33088a4a462a1d
Daniel Balieiro <107065917+Baliieiro@users.noreply.github.com>
2023-03-06 17:35:09 -0300

    tentando adicionar 'active' no form

--- src/componentes/Botao/index.js
+++ src/componentes/Botao/index.js
@@ -1,9 +1,24 @@
-import './botao.css'
+import { useState } from "react";
+import "./Botao.css";
 
-const Botao = ({texto}) => {
-    return <button className="botao">
-        {texto}
+const Botao = (props) => {
+  const [button, setButton] = useState(false);
+
+  function handleClick() {
+    setButton((button) => !button);
+    console.log("click");
+  }
+
+  let toggleClassCheck = button ? "visivel" : "";
+
+  return (
+    <button
+      className={`botao ${props.className} ${toggleClassCheck} `}
+      onClick={handleClick}
+    >
+      {props.children}
     </button>
-}
+  );
+};
 
-export default Botao
+export default Botao;
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,57 +1,88 @@
-import { useState } from 'react'
-import Botao from '../Botao'
-import CampoTexto from '../CampoTexto'
-import ListaSuspensa from '../ListaSuspensa'
-import './formulario.css'
+import { useState } from "react";
+import Botao from "../Botao";
+import Campo from "../Campo";
+import ListaSuspensa from "../ListaSuspensa";
+import "./formulario.css";
 
-const Formulario = ({aoCadastrar, times}) => {
+const Formulario = ({ aoCadastrar, times, aoCadastrarTime,visivel }) => {
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
+  const [novoTime, setNovoTime] = useState("");
+  const [corTime, setCorTime] = useState("");
 
-    const [nome, setNome] = useState('')
-    const [cargo, setCargo] = useState('')
-    const [imagem, setImagem] = useState('')
-    const [time, setTime] = useState('')
+  const aoSubmeter = (evento) => {
+    evento.preventDefault();
+    // console.log("form enviado", nome, cargo, imagem, time);
+    aoCadastrar({
+      nome,
+      cargo,
+      imagem,
+    });
+  };
 
-    const aoSubmeter = (evento) => {
-        evento.preventDefault()
-        console.log('form enviado', nome, cargo, imagem, time )
-        aoCadastrar({
-            nome,
-            cargo,
-            imagem,
-            time
-        })
-    }
+  return (
+    <section className="formulario-container">
+      <div className="div-form">
+        <form className="formulario" onSubmit={aoSubmeter}>
+          <h2>Preencha os dados para criar o card do colaborador.</h2>
+          <Campo
+            obrigatorio
+            label="Nome"
+            placeholder="Digite seu nome "
+            valor={nome}
+            aoAlterado={(valor) => setNome(valor)}
+          />
+          <Campo
+            obrigatorio
+            label="Cargo"
+            placeholder="Digite seu cargo "
+            valor={cargo}
+            aoAlterado={(valor) => setCargo(valor)}
+          />
+          <Campo
+            label="Imagem"
+            placeholder="Informe o endereço da imagem "
+            aoAlterado={(valor) => setImagem(valor)}
+          />
+          <ListaSuspensa
+            obrigatorio
+            label="Times"
+            items={times}
+            valor={time}
+            aoAlterado={(valor) => setTime(valor)}
+          />
+          <Botao>Criar card</Botao>
+        </form>
+        <form
+          className="formulario"
+          onSubmit={(evento) => {
+            evento.preventDefault();
+            aoCadastrarTime({ nome: novoTime, cor: corTime });
+          }}
+        >
+          <h2>Preencha os dados para criar um novo time.</h2>
+          <Campo
+            obrigatorio
+            label="Nome"
+            placeholder="Digite o nome do time "
+            valor={novoTime}
+            aoAlterado={(valor) => setNovoTime(valor)}
+          />
+          <Campo
+            type="color"
+            obrigatorionp
+            label="Cor do time"
+            placeholder="Digite a cor do time "
+            valor={corTime}
+            aoAlterado={(valor) => setCorTime(valor)}
+          />
+          <Botao>Criar um novo time</Botao>
+        </form>
+      </div>
+    </section>
+  );
+};
 
-    return (
-        <section className="formulario-container">
-            <form className="formulario" onSubmit={aoSubmeter}>
-                <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto
-                    obrigatorio={true}
-                    label='Nome'
-                    placeholder='Digite seu nome '
-                    valor={nome}
-                    aoAlterado={valor => setNome(valor)}/>
-                <CampoTexto
-                    obrigatorio={true}
-                    label='Cargo' 
-                    placeholder='Digite seu cargo '
-                    valor={cargo}
-                    aoAlterado={valor => setCargo(valor)}/>
-                <CampoTexto 
-                    label='Imagem' 
-                    placeholder='Informe o endereço da imagem '
-                    aoAlterado={valor => setImagem(valor)}/>
-                <ListaSuspensa 
-                    obrigatorio={true}
-                    label='Times'
-                    items={times} 
-                    valor={time}
-                    aoAlterado={valor => setTime(valor)}/>
-                <Botao texto='Criar card' />
-            </form>
-        </section>
-    )
-}
-
-export default Formulario
+export default Formulario;
